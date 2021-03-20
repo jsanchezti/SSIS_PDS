6c2d377b12215f849329a222dd9e923427baca76
jsanchezit <jared.axl@gmail.com>
2021-03-19 18:46:59 -0600

    IT 243 - Added LoadNAICSCodeHierDim_js.dtsx

--- stored_procedures/dbo.usp_LoadSSISConfigurations.sql
+++ stored_procedures/dbo.usp_LoadSSISConfigurations.sql
@@ -27,6 +27,7 @@ Ver      Date        Author           Description
 1.1      02/25/2021  JSANCHEZ         2. Added conn_DFNB3 Connection Configuration
 1.2      02/25/2021  JSANCHEZ         3. Added LoadDFNB3_js Configuration 
 1.3      03/08/2021  JSANCHEZ		  4. Added LoadEXM_js configuration
+1.4      03/08/2021  JSANCHEZ		  5. Added LoadNAICSCodeHierDim_js configuration
 
 
 RUNTIME: 
@@ -181,6 +182,27 @@ SELECT c.*
           (
            'LoadEXM_js'
 		 , 'C:\Users\jared\Desktop\LDSBC 2\OneDrive - Ensign College\Winter 2021\IT 243\week 01\EXM\EXM\txt_files'
+         , '\Package.Variables[User::v_data_share_root].Properties[Value]'
+         , 'String'
+          );
+
+
+	-- 3.4) LoadNAICSCodeHierDim_js
+
+    DELETE FROM dbo.[SSIS Configurations]
+     WHERE ConfigurationFilter = 'LoadNAICSCodeHierDim_js';
+	
+
+	-- 3.4.1) v_data_share_root
+
+    INSERT INTO dbo.[SSIS Configurations](ConfigurationFilter
+                                        , ConfiguredValue
+                                        , PackagePath
+                                        , ConfiguredValueType)
+    VALUES
+          (
+           'LoadNAICSCodeHierDim_js'
+		 , 'C:\Users\jared\Desktop\LDSBC 2\OneDrive - Ensign College\Winter 2021\IT 243\week 01\DFNB_dw\xls_files'
          , '\Package.Variables[User::v_data_share_root].Properties[Value]'
          , 'String'
           );
